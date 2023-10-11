c146ec1bad0bfe203da197ef622d5ff7d1204510
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-11 23:08:21 +0530

    authSlice completed

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState=[
+    {
+        user:null,
+        token:null
+    }
+];
+
+
+const authSlice=createSlice({
+    name:'auth',
+    initialState,
+    reducers:{
+        setCredential:(state,action)=>{
+            const {user,accessToken}=action.payload
+            state.user=user;
+            state.token=accessToken;
+        },
+        logOut:(state,action)=>{
+            state.user=null;
+            state.token=null;
+        }
+    }
+
+})
+
+export const{setCredential,logOut}=authSlice.actions;
+
+export default authSlice.reducer;
+
+export const selectCurrentUser =(state)=>state.authSlice.user;
+export const selectCurrentToken =(state)=>state.authSlice.token;
