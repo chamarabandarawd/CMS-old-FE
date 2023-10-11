c8f88b7701941caad5b0b894f0f949fc0609ab3a
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-11 23:44:46 +0530

    baseQueryWithReauth methode build in api slice

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -15,4 +15,29 @@ const baseQuery=fetchBaseQuery({
 })
 
 
-const apiSlice=createSlice({})
+const baseQueryWithReauth=async(args,api,extraOptions)=>{
+    let result = await baseQuery(args,api,extraOptions)
+
+    if(result?.error?.originalStatus===401){
+        console.log('sending refresh token')
+        //send refresh token to get new accesstoken
+        const refreshResult=await baseQuery('/userInfo/refreshToken',api,extraOptions)
+        console.log(refreshResult)
+        if(refreshResult?.data){
+            const user =api.getState().auth.user
+            //store the ne token
+            api.dispatch(setCredentails({...refreshResult.data,user}))
+            //retry the original query with new access token
+            result=await baseQuery(args,api,extraOptions)
+        }else{
+            api.dispatch(logOut());
+        }
+    }
+    return result;
+}
+
+
+export const apiSlice=createAPi({
+    baseQuery:baseQueryWithReauth,
+    endpoints:builder=>({})
+})
